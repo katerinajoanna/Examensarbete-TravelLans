a96811a1223c736f6c5fc7b411da6af8bcef6ae7
katerinajoanna <kasiapotar@gmail.com>
2025-02-09 20:27:21 +0100

    lite fixat

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,9 @@
-
+import logo from '../assets/logo-niebnieski2.svg';
 
 function Header() {
     return (
-        <div>
-
+        <div className="sticky top-0 left-0 bg-lime-400 w-full flex justify-start items-center gap-1 p-2 shadow-md z-10 ">
+            <img src={logo} className='w-24 p-1 animate-slideIn delay-150' alt="logo" />
         </div>
     );
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+import Header from "../components/Header";
 
 
 function HomePage() {
     return (
         <div>
+            <Header />
             <h1>Welcome</h1>
         </div>
     );
