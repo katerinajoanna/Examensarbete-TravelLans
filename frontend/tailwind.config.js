305e5f3965a19870d79953529a87c36d6fb5c3e7
katerinajoanna <kasiapotar@gmail.com>
2025-02-09 20:05:04 +0100

    lagt till colors i tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         bgLine: '#A8C33F',            // Linie oddzielające, tło headera, stroke
         textPrimary: '#031BBD',       // Główne teksty (np. nagłówki)
         textSecondary: '#E5A000',     // Podkreślenia, teksty dodatkowe
-        overlay: '#171B3D',           // Pasek przezroczysty na zdjęciu z nazwami kontynentów
+        bgOverlay: '#171B3D',           // Pasek przezroczysty na zdjęciu z nazwami kontynentów
         bgButton: '#2B336E',          // Tło przycisków
         bgNoteLight: '#D8E1B6',       // Tło jasne dla wpisywania notatek/opisów
         bgNoteDark: '#F2F4EA',        // Alternatywne tło dla notatek/opisów
