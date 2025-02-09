0ec9f9a19fbda4fc541e5b6e7b4048a7d0a73282
katerinajoanna <kasiapotar@gmail.com>
2025-02-09 16:22:18 +0100

    Add gradient to all pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,17 @@ export default {
   ],
   theme: {
     extend: {
-
+      colors: {
+        'bgPrimary': {
+          0: '#333E89',   // 0%
+          50: '#202756',   // 50%
+          75: '#171B3D',   // 75%
+          100: '#0D1023',  // 100%
+        }
+      },
+      backgroundImage: {
+        'gradient-primary': 'linear-gradient(90deg, #333E89 0%, #202756 50%, #171B3D 75%, #0D1023 100%)',
+      },
     },
   },
   plugins: [],
