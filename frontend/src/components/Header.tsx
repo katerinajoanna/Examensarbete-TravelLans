1efdab082c6da5d44c9ea9e6073bc4d80d92d9de
katerinajoanna <kasiapotar@gmail.com>
2025-02-10 23:24:51 +0100

    Ändrat logo, förberedde startsidan preliminärt

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,27 +1,25 @@
-import logo from '../assets/logo3.svg';
-import Terra from '../assets/Terra.svg';
-
-
+import logo from '../assets/logo1.svg';
+import Terra from '../assets/terralens.svg';
 
 function Header() {
     return (
-        <div className="sticky top-0 left-0 bg-lime-400 w-full flex justify-center items-center gap-1 p-2 shadow-md z-10">
+        <div className="sticky top-0 left-0 bg-lime-400 w-full flex justify-start items-center gap-1 p-2 shadow-md z-10">
             {/* Terra image */}
             <img
                 src={Terra}
-                className="w-56 h-auto p-1 animate-slideIn sm:w-48 md:w-56 lg:w-64 xl:w-72 max-w-full"
-                alt="Terra Lens"
+                alt="Terra"
+                className="p-1 ml-4 animate-slideIn max-w-full object-contain w-48 sm:w-56 md:w-64 lg:w-72"
             />
+
             {/* Logo image */}
             <img
                 src={logo}
-                className="w-24 p-1 animate-slideIn delay-150 sm:w-20 md:w-24 lg:w-28 xl:w-32 max-w-full"
                 alt="logo"
+                className="p-1 animate-slideIn delay-150 max-w-full object-contain w-12 sm:w-14 md:w-16 lg:w-18"
             />
         </div>
     );
 }
 
 export default Header;
-
 
