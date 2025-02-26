bcc228b063525478c10727d5bf284fab2f4eed15
katerinajoanna <kasiapotar@gmail.com>
2025-02-26 09:00:18 +0100

    fixat font-size i PlaceCard

--- frontend/src/components/gallery/PlaceCard.tsx
+++ frontend/src/components/gallery/PlaceCard.tsx
@@ -10,9 +10,9 @@ const PlaceCard: React.FC<PlaceCardProps> = ({ title, image, country, category, 
                 src={image}
                 alt={title}
                 className="w-full h-48 object-cover rounded-md shadow-cardShadow" />
-            <h3 className="text-xl text-textPrimary xl:text-2xl font-bold p-1 mt-2">{title}</h3>
-            <p className="text-base text-bgOverlay font-bold p-1">{country} - {category}</p>
-            <p className="mt-2 text-sm text-bgOverlay tracking-widest md:tracking-[.09rem] font-medium p-2">{description}</p>
+            <h3 className="text-xl text-textPrimary md:text-2xl xl:text-3xl font-bold p-1 mt-2">{title}</h3>
+            <p className="text-base md:text-lg xl:text-xl text-bgOverlay font-bold p-1">{country} - {category}</p>
+            <p className="mt-2 text-sm text-bgOverlay md:text-base xl:text-lg tracking-widest md:tracking-[.09rem] xl:tracking-wide font-medium font-robotoFlex p-2">{description}</p>
             <Link
                 to={linkInfo}
                 className="text-textPrimary hover:underline focus-visible:underline active:underline mt-2 flex p-1 gap-3 font-medium">
