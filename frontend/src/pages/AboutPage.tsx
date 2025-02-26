d2f5c7f0f20fc0508b794690175ee7a100f44048
katerinajoanna <kasiapotar@gmail.com>
2025-02-26 16:29:33 +0100

    fixat footer i About

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,11 +1,12 @@
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const AboutPage = () => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col min-h-screen">
             <Header title='About Us' />
-            <main className="text-center text-textMuted font-robotoSerif m-6">
+            <main className="flex-1 text-center text-textMuted font-robotoSerif m-6 md:m-8 lg:m-10 xl:m-[3.5rem]">
                 <h2 className="font-semibold text-xl sm:text-2xl md:text-3xl lg:text-4xl text-textMuted sm:leading-7 md:leading-8 lg:leading-[2.6rem] font-robotoSerif tracking-widest md:tracking-[.09rem] p-1 mt-8">
                     Welcome to Lans Travel!
                 </h2>
@@ -27,19 +28,19 @@ const AboutPage = () => {
                 <p className="text-left text-xs sm:text-base md:text-xl lg:text-xl text-textMuted leading-5 sm:leading-6 md:leading-8 lg:leading-[2rem] font-robotoSerif tracking-widest md:tracking-[.09rem] p-4 mt-4">
                     So, pack your virtual bags and start exploring with us. The wonders of the world are just a click away!
                 </p>
-                <div className="text-center text-xs sm:text-base md:text-xl lg:text-xl mt-8">
+                <div className="text-center text-xs sm:text-base md:text-xl lg:text-xl mt-8 mb-9 md:mb-[4rem]">
                     <p>Happy travels!</p>
                     <p>
                         The Lans Travels Team <span className="text-xl md:text-2xl lg:text-3xl">🌿</span>
                     </p>
                 </div>
+
             </main>
-
-            <div className="w-11/12 h-[0.5px] bg-bgLine mx-auto my-6"></div>
-
+            <div className="w-11/12 h-[0.5px] bg-bgLine mx-auto mb-2"></div>
             <Footer />
         </div>
     );
 };
 
 export default AboutPage;
+
