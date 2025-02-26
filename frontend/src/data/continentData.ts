4107a31599970f6fe4c7605d1c21dbc3e2b956df
katerinajoanna <kasiapotar@gmail.com>
2025-02-26 12:36:19 +0100

    Fixade buggar i navigeringen för Nord- och Sydamerika

--- frontend/src/data/continentData.ts
+++ frontend/src/data/continentData.ts
@@ -1,11 +1,33 @@
 import canion from '../assets/images/Canyonlands-National-Park-Utah.jpg';
 import jakies from '../assets/images/zhangjiajie-d485479.jpg';
 import niagara from '../assets/images/Niagara-cud-natury-w-nocy-.jpg';
+import AfricaVideo from '../assets/videos/afrykaVideo.mp4';
+import NorthVideo from '../assets/videos/ampnVideo.mp4';
+import SouthVideo from '../assets/videos/ampndVideo.mp4';
+import AsiaVideo from '../assets/videos/azjaVideo.mp4';
+import EuropeVideo from '../assets/videos/europaVideo.mp4';
+import OceaniaVideo from '../assets/videos/australiaVideo.mp4';
 
-export const continentData = {
+export const continentData: Record<string, {
+    //indeksacja data
+    name: string;
+    description: string;
+    video: string;
+    places: {
+        id: number;
+        title: string;
+        country: string;
+        category: string;
+        image: string;
+        description: string;
+        linkInfo: string;
+    }[];
+}> =
+{
     africa: {
         name: "Africa",
         description: "Discover the amazing nature of Africa",
+        video: AfricaVideo,   // potem url z S3
         places: [
             {
                 id: 1,
@@ -29,7 +51,8 @@ export const continentData = {
     },
     asia: {
         name: "Asia",
-        description: "Discover the amazing nature of Africa of Asia.",
+        description: "Discover the amazing nature of Asia.",
+        video: AsiaVideo,
         places: [
             {
                 id: 3,
@@ -87,12 +110,59 @@ export const continentData = {
             },
         ],
     },
-    oceania: {
-        name: "Oceania",
-        description: "Explore the stunning landscapes of Oceania.",
+
+    europe: {
+        name: "Europe",
+        description: "Discover the amazing nature of Europe.",
+        video: EuropeVideo,
         places: [
             {
                 id: 9,
+                title: "Mount Fuji",
+                country: "Japan",
+                category: "Mountain",
+                image: niagara,
+                description: "Japan’s highest and most iconic mountain.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+            {
+                id: 10,
+                title: "Ha Long Bay",
+                country: "Vietnam",
+                category: "Bay",
+                image: jakies,
+                description: "A UNESCO World Heritage Site known for its limestone islands. A UNESCO World Heritage Site known for its limestone islands. A UNESCO World Heritage Site known for its limestone islands. A UNESCO World Heritage Site known for its limestone islands. A UNESCO World Heritage Site known for its limestone islands.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+            {
+                id: 11,
+                title: "Mount Fuji",
+                country: "Japan",
+                category: "Mountain",
+                image: canion,
+                description: "Japan’s highest and most iconic mountain.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+            {
+                id: 12,
+                title: "Ha Long Bay",
+                country: "Vietnam",
+                category: "Bay",
+                image: jakies,
+                description: "A UNESCO World Heritage Site known for its limestone islands. A UNESCO World Heritage Site known for its limestone islands.A UNESCO World Heritage Site known for its limestone islands.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+
+        ],
+    },
+
+    oceania: {
+        name: "Oceania",
+        description: "Explore the stunning landscapes of Oceania.",
+        video: OceaniaVideo,
+        places: [
+            {
+                id: 13,
                 title: "Victoria Falls",
                 country: "Zimbabwe",
                 category: "Waterfall",
@@ -101,7 +171,57 @@ export const continentData = {
                 linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
             },
             {
-                id: 10,
+                id: 14,
+                title: "Sahara Desert",
+                country: "Morocco",
+                category: "Desert",
+                image: jakies,
+                description: "The largest hot desert in the world.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+        ],
+    },
+    northamerica: {
+        name: "North America",
+        description: "Explore the stunning landscapes of North America.",
+        video: NorthVideo,
+        places: [
+            {
+                id: 15,
+                title: "Victoria Falls",
+                country: "Zimbabwe",
+                category: "Waterfall",
+                image: niagara,
+                description: "One of the largest and most famous waterfalls in the world.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+            {
+                id: 16,
+                title: "Sahara Desert",
+                country: "Morocco",
+                category: "Desert",
+                image: jakies,
+                description: "The largest hot desert in the world.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+        ],
+    },
+    southamerica: {
+        name: "South America",
+        description: "Explore the stunning landscapes of South America.",
+        video: SouthVideo,
+        places: [
+            {
+                id: 17,
+                title: "Victoria Falls",
+                country: "Zimbabwe",
+                category: "Waterfall",
+                image: niagara,
+                description: "One of the largest and most famous waterfalls in the world.",
+                linkInfo: "https://pl.wikipedia.org/wiki/Andy ",
+            },
+            {
+                id: 18,
                 title: "Sahara Desert",
                 country: "Morocco",
                 category: "Desert",
