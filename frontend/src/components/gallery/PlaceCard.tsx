80c9746dbe21c3683513146c6f8071ba10f050cd
katerinajoanna <kasiapotar@gmail.com>
2025-02-27 12:09:33 +0100

    lite fix

--- frontend/src/components/gallery/PlaceCard.tsx
+++ frontend/src/components/gallery/PlaceCard.tsx
@@ -9,7 +9,7 @@ const PlaceCard: React.FC<PlaceCardProps> = ({ title, image, country, category, 
             <img
                 src={image}
                 alt={title}
-                className="w-full h-48 object-cover rounded-md shadow-cardShadow" />
+                className="w-full h-48 object-cover rounded-md shadow-cardShadow border border-bgLine" />
             <h3 className="text-xl text-textPrimary md:text-2xl xl:text-3xl font-bold p-1 mt-2">{title}</h3>
             <p className="text-base md:text-lg xl:text-xl text-bgOverlay font-bold p-1">{country} - {category}</p>
             <p className="mt-2 text-sm text-bgOverlay md:text-base xl:text-lg tracking-widest md:tracking-[.09rem] xl:tracking-wide font-medium font-robotoFlex p-2">{description}</p>
--- frontend/src/pages/ContinentPage.tsx
+++ frontend/src/pages/ContinentPage.tsx
@@ -25,7 +25,7 @@ const ContinentPage: React.FC = () => {
 
             <main className="flex-1 max-w-7xl p-4 mx-4 sm:mx-8 md:mx-14 lg:mx-18 xl:mx-auto">
 
-                <p className="text-xl text-center  md:text-3xl lg:text-4xl xl:text-4xl text-textSecondary font-bold italic tracking-wide p-3 md:p-5 lg:p-6 xl:p-6 mt-6">{continent.description}</p>
+                <p className="text-xl text-center sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl text-textSecondary stroke-gallery font-bold italic tracking-wide p-3 md:p-5 lg:p-6 xl:p-6 mt-6">{continent.description}</p>
 
                 <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     {continent.places.map((place) => (
