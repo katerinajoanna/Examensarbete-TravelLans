1332a4d190374b5ba3a98a0401b735cbf9cac012
katerinajoanna <kasiapotar@gmail.com>
2025-02-27 11:39:27 +0100

    Justerat textstroke för rubriktext

--- frontend/src/components/gallery/GallerySection.tsx
+++ frontend/src/components/gallery/GallerySection.tsx
@@ -7,7 +7,7 @@ const Galleries = () => {
 
     return (
         <section className='max-w-7xl mx-auto p-3'>
-            <h2 className='text-3xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-7xl text-textSecondary text-center font-robotoSans italic font-bold tracking-wider stroke-gallery mb-8 md:mb-12 lg:mb-14'>
+            <h2 className='text-3xl sm:text-4xl md:text-5xl lg:text-7xl xl:text-7xl text-textSecondary text-center font-robotoFlex italic font-bold tracking-wider stroke-gallery mb-8 md:mb-12 lg:mb-14'>
                 Galleries
             </h2>
             <div className="grid grid-cols-1 font-robotoFlex tracking-[0.05em] sm:grid-cols-2 lg:grid-cols-3 gap-6 text-2xl">
