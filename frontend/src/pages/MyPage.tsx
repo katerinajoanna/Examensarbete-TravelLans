ac19fae2f3551604b060980d8f1b79ef6ff233c6
katerinajoanna <kasiapotar@gmail.com>
2025-02-27 20:43:06 +0100

    Lade till formulär för registrering och inloggning, samt knappar i LoginRegister.tsx. Lade också till shadow i tailwind.config för knappar och formulär

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,12 +1,17 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 
 const MyPage = () => {
 
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Header title='My page' />
+            <main className="flex-1 max-w-7xl p-4 mx-4 sm:mx-8 md:mx-14 lg:mx-18 xl:mx-auto">
 
+            </main>
+            <div className="w-11/12 h-[0.5px] bg-bgLine mx-auto mb-2"></div>
+            <Footer />
         </div>
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,12 +16,14 @@ export default {
         bgNoteLight: '#D8E1B6',       // Tło jasne dla wpisywania notatek/opisów
         bgNoteDark: '#F2F4EA',        // Alternatywne tło dla notatek/opisów
         textMuted: '#F0EFE8',        // Kolor tekstu na jasnych tłach
-
+        borderInput: '#2c6c9a',
       },
       boxShadow: {
         imgShadow: '-6px 6px 12px rgba(169, 166, 146, 0.45)', // images shadow
         searchShadow: '0px 4px 10px rgba(198, 195, 63, 0.25)',   //search shadow
         cardShadow: '0px 6px 15px rgba(0, 0, 0, 0.3)',
+        formShadow: '6px 6px 12px rgba(169, 166, 146, 0.45)',
+        btnShadow: '4px 4px 8px rgba(169, 166, 146, 0.45)',
       },
       fontFamily: {
         robotoSerif: ['Roboto Serif', 'serif'],
