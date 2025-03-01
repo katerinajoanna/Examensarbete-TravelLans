e456572e42fad7fc468c5212daa7b7ef1406b56d
katerinajoanna <kasiapotar@gmail.com>
2025-03-01 10:41:19 +0100

    fixat register- och loginform, btn i LoginRegister

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,7 +6,7 @@ import Twitter from "./iconsSvg/Twitter";
 const Footer: React.FC = () => {
     return (
         <footer className='bg-bgPrimary text-bgLine  text-base md:text-lg lg:text-2xl p-5'>
-            <div className='flex flex-col sm:flex-row justify-between items-center gap-[.1rem] mx-8'>
+            <div className='flex flex-col sm:flex-row justify-between items-center gap-[.1rem] mx-8 xl:mx-14'>
 
                 <div className='flex items-center text gap-0.5 font-robotoFlex italic p-1'>
                     <p className='text-[#ADDA4A]'>&copy; 2025 Trawel Lans</p>
--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -5,19 +5,19 @@ const RegisterForm = () => {
             <form className="space-y-2">
                 <div>
                     <label className="block">Username</label>
-                    <input type="text" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full" placeholder="Unique username (e.g. jan123)" />
+                    <input type="text" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full rounded" placeholder="Unique username (e.g. jan123)" />
                 </div>
                 <div>
                     <label className="block">Email</label>
-                    <input type="email" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full" />
+                    <input type="email" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full rounded" />
                 </div>
                 <div>
                     <label className="block">Password</label>
-                    <input type="password" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full" />
+                    <input type="password" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full rounded" />
                 </div>
                 <div>
                     <label className="block">Confirm Password</label>
-                    <input type="password" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full" />
+                    <input type="password" className="border border-bgLine focus:border-borderInput focus:outline-none p-2 w-full rounded" />
                 </div>
                 <div className="flex items-center">
                     <input type="checkbox" className="mr-2 h-5 w-5 cursor-pointer appearance-none border border-bgLine rounded-sm checked:bg-lime-500 checked:border-transparent focus:ring-1 focus:ring-bgLine before:content-['✔'] before:text-textMuted before:text-xs before:flex before:items-center before:justify-center" required />
